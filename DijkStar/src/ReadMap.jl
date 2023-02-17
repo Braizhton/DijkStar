06f8dad8ced8848c005b7608319c5c43ef313255
Braizhton <arthur.asd8@hotmail.fr>
2023-02-17 12:31:09 +0100

    Implement ReadMap to translate map file to map Matrix

---
+++ DijkStar/src/ReadMap.jl
@@ -0,0 +1,35 @@
+function read_map(map::String)
+    mapInput::IOStream = open(map, "r")
+
+    s::String = readline(mapInput) # Reading the first line and testing file format
+    if s != "type octile"
+        print("Wrong file format !")
+        return
+    end
+
+    # Reading line with height value
+    height::Int64 = parse(Int64, readline(mapInput)[8:end]) 
+    # Reading line with width value
+    width::Int64 = parse(Int64, readline(mapInput)[7:end])
+
+    print("Height : ", width, "\n")
+    print("Width : ", height, "\n")
+
+    trash = readline(mapInput)
+
+    mapMatrix::Matrix{Char} = Matrix{Char}(undef, height, width)
+   
+    i::Int64 = 1
+    for line in eachline(mapInput)
+        mapMatrix[i,:] = collect(line)
+        i = i + 1
+    end
+
+    return mapMatrix
+        
+    # mapInput = open(ARGS[1], "r")
+
+end
+
+# read_map(ARGS[1])
+# Luxor / Makie (graphique) 
